bb3f605510af94bdeb472f8dcae0fffea3d25c95
Italo Rayone <kirito@eniac.mds.net>
2020-01-28 14:35:03 -0300

    Adding footer to the page

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 // Mobile
 const navSlide = () => {
-   const burguer = $('.burguer')
-   const nav = $('.nav-links')
-   const navLinks = $('.nav-links li')
+    const burguer = $('.burguer')
+    const nav = $('.nav-links')
+    const navLinks = $('.nav-links li')
 
-   // Active Nav on mophile
-   burguer.click(() => {
-      nav.toggleClass('nav-active')
+    // Active Nav on mophile
+    burguer.click(() => {
+        nav.toggleClass('nav-active')
 
-      // Animate Links
-      navLinks.each((index) => {
-         $(this).css("animation", `navLinkFade 0.5s ease forwards`)
-      })
+        // Animate Links
+        navLinks.each((index) => {
+            $(this).css("animation", `navLinkFade 0.5s ease forwards`)
+        })
 
-      // Animate Burguer
-      burguer.toggleClass('toggle')
-   })
+        // Animate Burguer
+        burguer.toggleClass('toggle')
+    })
 
 }
 
 
 
 
-const app = ()=> {
-   navSlide()
+const app = () => {
+    navSlide()
 }
 
 app()
